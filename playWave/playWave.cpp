90ca5528f5d7fcc95060adb1143cecbe5ad9f6c7
simonscott <simonscott@berkeley.edu>
2014-10-28 00:21:12 -0700

    Removing old file.

--- playWave/playWave.cpp
+++ playWave/playWave.cpp
@@ -1,9 +1,5 @@
-// playWave.cpp : Defines the entry point for the console application.
-//
-
 #include <iostream>
-#include <iostream>
-#include <fstream>
+#include "../WaveFile/WaveFile.h"
 #include "portaudio.h"
 
 using namespace std;
@@ -12,90 +8,31 @@ using namespace std;
 #define SAMPLE_RATE   (44100)
 #define FRAMES_PER_BUFFER  (64)
 
-#ifndef M_PI
-#define M_PI  (3.14159265)
-#endif
 
-
-// Structure to hold the wavefile
-struct WaveFile
+class PlayWave
 {
 public:
 
-    static const unsigned short NUM_CHARS = 4;
-
-public:
-
-    WaveFile() : Data(nullptr) {}
-    ~WaveFile() { delete[] Data; }
-
-    char ChunkID[NUM_CHARS];
-    unsigned int ChunkSize;
-    char Format[NUM_CHARS];
-    char SubChunkID[NUM_CHARS];
-    unsigned int SubChunkSize;
-    unsigned short AudioFormat;
-    unsigned short NumChannels;
-    unsigned int SampleRate;
-    unsigned int ByteRate;
-    unsigned short BlockAlign;
-    unsigned short BitsPerSample;
-    char SubChunk2ID[NUM_CHARS];
-    unsigned int SubChunk2Size;
-    unsigned char* Data; 
-};
-
-
-class Sine
-{
-public:
+	// Member variables
     PaStream *stream;
-    WaveFile waveFile;
-    unsigned int sample_count;
-    char message[20];
+	WaveFile* wave;
+	char message[100];
 
-	Sine()
+	/**
+	 * Constructor
+	 */
+	PlayWave(string filename)
     {
 		stream = 0;
-		sample_count = 0;
 
-		cout << "Loading wave file" << endl;
-		ifstream file("D:\\Programming\\dj_gestures\\myoDJ\\media\\this_is_dj_simon.wav", ios::binary);
-
-		if(file.good())
-			cout << "File loaded." << endl;
-		else
-			cout << "File load FAILED." << endl;
-
-		if (file.good())
-		{
-			file.read(waveFile.ChunkID, WaveFile::NUM_CHARS);
-			file.read(reinterpret_cast<char*>(&waveFile.ChunkSize), sizeof(unsigned int));
-			file.read(waveFile.Format, WaveFile::NUM_CHARS);
-			file.read(waveFile.SubChunkID, WaveFile::NUM_CHARS);
-			file.read(reinterpret_cast<char*>(&waveFile.SubChunkSize), sizeof(unsigned int));
-			file.read(reinterpret_cast<char*>(&waveFile.AudioFormat), sizeof(unsigned short));
-			file.read(reinterpret_cast<char*>(&waveFile.NumChannels), sizeof(unsigned short));
-			file.read(reinterpret_cast<char*>(&waveFile.SampleRate), sizeof(unsigned int));
-			file.read(reinterpret_cast<char*>(&waveFile.ByteRate), sizeof(unsigned int));
-			file.read(reinterpret_cast<char*>(&waveFile.BlockAlign), sizeof(unsigned short));
-			file.read(reinterpret_cast<char*>(&waveFile.BitsPerSample), sizeof(unsigned short));
-			file.read(waveFile.SubChunk2ID, WaveFile::NUM_CHARS);
-			file.read(reinterpret_cast<char*>(&waveFile.SubChunk2Size), sizeof(unsigned int));
-			waveFile.Data = new unsigned char[waveFile.SubChunk2Size];
-			file.read(reinterpret_cast<char*>(waveFile.Data), sizeof(waveFile.SubChunk2Size));
-			file.close();
-		}
-
-		// Print size of audio data
-	    cout << "Num chan: " << waveFile.NumChannels << "Samp Rate: " << waveFile.SampleRate << endl;
-		cout << "Data section is " << waveFile.SubChunk2Size << "bytes." << endl;
-
-		for(int i = 0; i < 1000; i++)
-			cout << reinterpret_cast<short*>(waveFile.Data)[i] << endl;
-
+		// Read the header of the wavefile
+		wave = new WaveFile(filename);
     }
 
+
+	/**
+	 * Open the specified output audio device (i.e. speakers)
+	 */
     bool open(PaDeviceIndex index)
     {
         PaStreamParameters outputParameters;
@@ -106,7 +43,7 @@ public:
         }
 
         outputParameters.channelCount = 2;       /* stereo output */
-        outputParameters.sampleFormat = paFloat32; /* 16 bit int output */
+        outputParameters.sampleFormat = paFloat32;
         outputParameters.suggestedLatency = Pa_GetDeviceInfo( outputParameters.device )->defaultLowOutputLatency;
         outputParameters.hostApiSpecificStreamInfo = NULL;
 
@@ -117,23 +54,19 @@ public:
             SAMPLE_RATE,
             FRAMES_PER_BUFFER,
             paClipOff,      /* we won't output out of range samples so don't bother clipping them */
-            &Sine::paCallback,
+            &PlayWave::paCallback,
             this            /* Using 'this' for userData so we can cast to Sine* in paCallback method */
             );
 
         if (err != paNoError)
-        {
-            /* Failed to open stream to device !!! */
             return false;
-        }
 
-        err = Pa_SetStreamFinishedCallback( stream, &Sine::paStreamFinished );
+        err = Pa_SetStreamFinishedCallback( stream, &PlayWave::paStreamFinished );
 
         if (err != paNoError)
         {
             Pa_CloseStream( stream );
             stream = 0;
-
             return false;
         }
 
@@ -158,7 +91,6 @@ public:
             return false;
 
         PaError err = Pa_StartStream( stream );
-
         return (err == paNoError);
     }
 
@@ -168,9 +100,9 @@ public:
             return false;
 
         PaError err = Pa_StopStream( stream );
-
         return (err == paNoError);
     }
+
 
 private:
 
@@ -181,23 +113,13 @@ private:
         PaStreamCallbackFlags statusFlags)
     {
         float *out = (float*)outputBuffer;
-        unsigned long i;
 
         (void) timeInfo; /* Prevent unused variable warnings. */
         (void) statusFlags;
         (void) inputBuffer;
 
-        for( i=0; i<framesPerBuffer; i++ )
-        {
-            //*out++ = static_cast <float> (rand()) / static_cast <float> (RAND_MAX); //((short*)waveFile.Data)[sample_count];
-            //*out++ = static_cast <float> (rand()) / static_cast <float> (RAND_MAX); //((short*)waveFile.Data)[sample_count];
-
-            *out++ = (float) ( ((short*)waveFile.Data)[sample_count] );
-            *out++ = (float) ( ((short*)waveFile.Data)[sample_count] );
-
-			sample_count++;
-            if( sample_count >= waveFile.SubChunk2Size/2 ) sample_count = 0;
-        }
+		// Get the next set of wave samples
+		wave->getSamples(framesPerBuffer * 2, out);
 
         return paContinue;
 
@@ -215,7 +137,7 @@ private:
     {
         /* Here we cast userData to Sine* type so we can call the instance method paCallbackMethod, we can do that since 
            we called Pa_OpenStream with 'this' for userData */
-        return ((Sine*)userData)->paCallbackMethod(inputBuffer, outputBuffer,
+        return ((PlayWave*)userData)->paCallbackMethod(inputBuffer, outputBuffer,
             framesPerBuffer,
             timeInfo,
             statusFlags);
@@ -232,7 +154,7 @@ private:
      */
     static void paStreamFinished(void* userData)
     {
-        return ((Sine*)userData)->paStreamFinishedMethod();
+        return ((PlayWave*)userData)->paStreamFinishedMethod();
     }
 
 };
@@ -242,24 +164,21 @@ private:
 int main(void)
 {
     PaError err;
-    Sine sine;
+    PlayWave playWave("..\\media\\this_is_dj_simon.wav");
 
-    printf("PortAudio Test: output sine wave. SR = %d, BufSize = %d\n", SAMPLE_RATE, FRAMES_PER_BUFFER);
-    
     err = Pa_Initialize();
     if( err != paNoError ) goto error;
 
-    if (sine.open(Pa_GetDefaultOutputDevice()))
+    if (playWave.open(Pa_GetDefaultOutputDevice()))
     {
-        if (sine.start())
+        if (playWave.start())
         {
             printf("Play for %d seconds.\n", NUM_SECONDS );
             Pa_Sleep( NUM_SECONDS * 1000 );
-
-            sine.stop();
+            playWave.stop();
         }
 
-        sine.close();
+        playWave.close();
     }
 
     Pa_Terminate();
